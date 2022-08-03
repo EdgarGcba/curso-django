e5f863005eb16a2d531b74e2eb9ef8e2d7e17dd6
Edgar Galleguillo <edgar.galleguillo@gmail.com>
2022-08-02 21:18:14 -0300

    rutas y plantillas

--- plantillas/plantillas/urls.py
+++ plantillas/plantillas/urls.py
@@ -6,4 +6,6 @@ from plantillas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('simple/', views.simple, name='simple'),
+    path('dinamico/<str:name>', views.dinamico, name='dinamico'),
+    
 ]
